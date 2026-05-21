e449249b6d23a190b538d3205d24cea87538159f
Z User <z@container>
2026-05-21 01:00:56 +0000

    v0.0.43 - NDK build with llama.cpp native library (arm64-v8a)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.apk.claw.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 42
-        versionName = "0.0.42"
+        versionCode = 43
+        versionName = "0.0.43"
         buildConfigField("String", "VERSION_INFO", getVersionGit())
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
